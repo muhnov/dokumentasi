a8e9e31c34867559969a2da989af9d91aa7eb466
muhnov <muh.nov004@gmail.com>
2025-04-14 10:38:27 +0700

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import nextra from 'nextra'
+ 
+const withNextra = nextra({
+  // ... Other Nextra config options
+})
+ 
+// You can include other Next.js configuration options here, in addition to Nextra settings:
+export default withNextra({
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/home',
+                permanent: true,
+            },
+        ]
+    }
+  // ... Other Next.js config options
+})
---
+++ src/app/_meta.js
@@ -0,0 +1,4 @@
+export default {
+    home:"Perkenalan",
+    about:"Dokumentasi",
+}
